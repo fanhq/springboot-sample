cdfa9bb5d2295e92a1d801f954ceb184110b8292
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-03-11 17:29:09 +0800

    spring boot grpc

--- security-rest-api/src/main/java/com/fanhq/example/entity/Role.java
+++ security-rest-api/src/main/java/com/fanhq/example/entity/Role.java
@@ -1,18 +1,11 @@
 package com.fanhq.example.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 /**
  * @author fanhaiqiu
  * @date 2019/2/26
  */
-@Entity
-@Table(name = "role")
 public class Role extends BaseEntity {
 
-    @Column(name = "role_name", nullable = true, length = 64)
     private String roleName;
 
     public String getRoleName() {
