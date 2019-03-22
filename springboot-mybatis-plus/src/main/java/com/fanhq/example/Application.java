f7ffd1227ac067e0a3e71da943629faddb43ae9f
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-03-22 15:28:40 +0800

    springboot mybatis

---
+++ springboot-mybatis-plus/src/main/java/com/fanhq/example/Application.java
@@ -0,0 +1,11 @@
+package com.fanhq.example;
+
+/**
+ * Hello world!
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ springboot-mybatis-plus/src/test/java/com/fanhq/example/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.fanhq.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
