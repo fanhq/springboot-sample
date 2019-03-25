5841e02f70f231c1b6c63ca311d895d6c71c782d
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-03-25 11:44:59 +0800

    mybatis-plus

--- springboot-mybatis-plus/src/main/java/com/fanhq/example/Application.java
+++ springboot-mybatis-plus/src/main/java/com/fanhq/example/Application.java
@@ -1,6 +1,5 @@
 package com.fanhq.example;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-@MapperScan("com.fanhq.example.mapper")
 public class Application {
 
     public static void main(String[] args) {
---
+++ springboot-mybatis-plus/src/main/java/com/fanhq/example/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.fanhq.example.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author fanhaiqiu
+ * @date 2019/3/25
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.fanhq.example.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
