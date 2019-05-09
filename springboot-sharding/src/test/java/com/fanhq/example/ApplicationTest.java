c4d0c2c720a1247b7392560e3f665269a381d22f
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-09 17:07:04 +0800

    springboot-sharding

--- springboot-sharding/src/test/java/com/fanhq/example/ApplicationTest.java
+++ springboot-sharding/src/test/java/com/fanhq/example/ApplicationTest.java
@@ -1,12 +1,12 @@
 package com.fanhq.example;
 
-import com.fanhq.example.entity.OrderEntity;
-import com.fanhq.example.repository.OrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 /**
  * @author fanhaiqiu
@@ -18,19 +18,11 @@ public class ApplicationTest {
 
 
     @Autowired
-    private OrderRepository orderRepository;
-
+    private DataSource dataSource;
 
     @Test
-    public void orderTest() {
-        for (long i = 1; i <= 40L; i++) {
-            OrderEntity order = new OrderEntity();
-            order.setId((i));
-            order.setStatus("order" + i);
-            order.setUserId(i + 1);
-            orderRepository.save(order);
-        }
-        assert true;
+    public void baseTest() {
+        assert dataSource != null;
     }
 
 }
