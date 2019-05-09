35751c9118d2eebaffa22ebf94d014ede82b7427
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-09 10:13:09 +0800

    springboot-sharding

---
+++ springboot-sharding/src/test/java/com/fanhq/example/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.fanhq.example;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author fanhaiqiu
+ * @date 2019/5/9
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+}
