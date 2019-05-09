a15209e51572ff987294843f84c623c36963f3ae
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-09 22:12:02 +0800

    springboot-sharding

--- springboot-sharding/src/main/java/com/fanhq/example/repository/OrderRepository.java
+++ springboot-sharding/src/main/java/com/fanhq/example/repository/OrderRepository.java
@@ -3,10 +3,19 @@ package com.fanhq.example.repository;
 import com.fanhq.example.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author fanhaiqiu
  * @date 2019/5/9
  */
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    /**
+     * 模糊查询
+     * @param pattern
+     * @return
+     */
+    List<OrderEntity> findByStatusIsLike(String pattern);
 
 }
