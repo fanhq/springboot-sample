4b9044998cb648c8321264a3baa8a3757aea90b1
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-23 15:29:36 +0800

    springboot-mybatis-tk

--- springboot-mybatis-tk/src/test/java/com/fanhq/example/ApplicationTest.java
+++ springboot-mybatis-tk/src/test/java/com/fanhq/example/ApplicationTest.java
@@ -1,11 +1,15 @@
 package com.fanhq.example;
 
 import com.fanhq.example.mapper.UsersMapper;
+import com.fanhq.example.model.Users;
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @author fanhaiqiu
@@ -18,10 +22,10 @@ public class ApplicationTest {
     @Autowired
     private UsersMapper usersMapper;
 
-
     @Test
-    public void mapperTest(){
-
+    public void mapperTest() {
+        List<Users> users = usersMapper.selectAll();
+        assert CollectionUtils.isNotEmpty(users);
     }
 
 }
