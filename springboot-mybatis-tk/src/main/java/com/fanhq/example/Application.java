9d0514d680f14824dc2d1fee975dc2148e2a7034
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-23 15:28:06 +0800

    springboot-mybatis-plus

--- springboot-mybatis-tk/src/main/java/com/fanhq/example/Application.java
+++ springboot-mybatis-tk/src/main/java/com/fanhq/example/Application.java
@@ -2,12 +2,14 @@ package com.fanhq.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author fanhaiqiu
  */
 
 @SpringBootApplication
+@MapperScan("com.fanhq.example.mapper")
 public class Application {
 
     public static void main(String[] args) {
--- springboot-mybatis-tk/src/test/java/com/fanhq/example/ApplicationTest.java
+++ springboot-mybatis-tk/src/test/java/com/fanhq/example/ApplicationTest.java
@@ -1,6 +1,9 @@
 package com.fanhq.example;
 
+import com.fanhq.example.mapper.UsersMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,5 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = Application.class)
 public class ApplicationTest {
 
+    @Autowired
+    private UsersMapper usersMapper;
+
+
+    @Test
+    public void mapperTest(){
+
+    }
 
 }
