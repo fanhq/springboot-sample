f367302c4a6eac866b9e6042ed91509613040a1c
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-24 10:42:14 +0800

    springboot-oauth2

--- springboot-oauth2-jwt/oauth2-server/src/main/java/com/fanhq/example/config/OAuth2AuthorizationServerConfig.java
+++ springboot-oauth2-jwt/oauth2-server/src/main/java/com/fanhq/example/config/OAuth2AuthorizationServerConfig.java
@@ -1,10 +1,8 @@
 package com.fanhq.example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -22,14 +20,11 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore())
-                .accessTokenConverter(accessTokenConverter())
-                .authenticationManager(authenticationManager);
+                .accessTokenConverter(accessTokenConverter());
     }
 
     @Bean
