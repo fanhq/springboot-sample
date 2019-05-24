2309ab5c0cf4f06dedce6ec3751ce074efbb199b
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-05-24 10:24:13 +0800

    springboot-oauth2

---
+++ springboot-oauth2-jwt/oauth2-resource/src/main/java/com/fanhq/example/config/CustomAccessTokenConverter.java
@@ -0,0 +1,23 @@
+package com.fanhq.example.config;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author fanhaiqiu
+ * @date 2019/5/24
+ */
+@Component
+public class CustomAccessTokenConverter extends DefaultAccessTokenConverter {
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
+        OAuth2Authentication authentication = super.extractAuthentication(claims);
+        authentication.setDetails(claims);
+        return authentication;
+    }
+
+}
