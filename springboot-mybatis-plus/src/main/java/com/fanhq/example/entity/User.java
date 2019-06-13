250f53ecc1a2a2c8052cd6f3c854c79c03b72ea7
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-06-13 15:44:47 +0800

    springboot-oauth2

--- springboot-mybatis-plus/src/main/java/com/fanhq/example/entity/User.java
+++ springboot-mybatis-plus/src/main/java/com/fanhq/example/entity/User.java
@@ -1,15 +1,20 @@
 package com.fanhq.example.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+
+import java.sql.Date;
 
 /**
  * @author fanhaiqiu
  * @date 2019/3/22
  */
 @Data
+@TableName("users")
 public class User {
     private Long id;
     private String name;
     private Integer age;
     private String email;
+    private Date createTime;
 }
--- springboot-mybatis-plus/src/test/java/com/fanhq/example/service/UserServiceImplTest.java
+++ springboot-mybatis-plus/src/test/java/com/fanhq/example/service/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.fanhq.example.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fanhq.example.Application;
 import com.fanhq.example.entity.User;
 import com.fanhq.example.mapper.UserMapper;
@@ -8,6 +9,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+import java.util.List;
 
 /**
  * @author fanhaiqiu
@@ -33,5 +37,13 @@ public class UserServiceImplTest {
     public void userMapperTest(){
         User user = userMapper.selectById(1);
         System.out.println(user.getId());
+    }
+
+    @Test
+    public void wraperTest(){
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.gt("create_time", new Date(System.currentTimeMillis()));
+        List<User> users = userMapper.selectList(queryWrapper);
+        System.out.println(users.size());
     }
 }
