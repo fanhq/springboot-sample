05e01f936e566d152bdd7af6ab7cd86f9f01a451
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-06-17 15:17:25 +0800

    springboot-mybatis-plus

--- springboot-batch/src/main/java/com/fanhq/example/core/Writer.java
+++ springboot-batch/src/main/java/com/fanhq/example/core/Writer.java
@@ -13,7 +13,7 @@ public class Writer implements ItemWriter<String> {
     @Override
     public void write(List<? extends String> messages) throws Exception {
         for (String msg : messages) {
-            System.out.println("Writing the data " + msg);
+            System.out.println("Writing the data :" + msg);
         }
     }
 
