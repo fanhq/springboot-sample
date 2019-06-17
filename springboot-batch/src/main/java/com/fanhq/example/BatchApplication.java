e1acc51e7e28e8273bed5a312b4e447855bd1305
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-06-17 14:46:26 +0800

    springboot-mybatis-plus

---
+++ springboot-batch/src/main/java/com/fanhq/example/BatchApplication.java
@@ -0,0 +1,16 @@
+package com.fanhq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fanhaiqiu
+ */
+
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
---
+++ springboot-batch/src/test/java/com/fanhq/example/BatchApplicationTest.java
@@ -0,0 +1,11 @@
+package com.fanhq.example;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author fanhaiqiu
+ * @date 2019/6/17
+ */
+public class BatchApplicationTest {
+
+}
